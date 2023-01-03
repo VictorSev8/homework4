101c3c646b7d0a4b839d64829a61e7c8a8044f8a
Sever <Severinv@list.ru>
2023-01-03 16:51:59 +0300

    Выполнено ДЗ 4. Условные операторы 2

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         // task 3
         System.out.println("Задача 3");
         int year = 2024;
-        if (year % 4 == 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
